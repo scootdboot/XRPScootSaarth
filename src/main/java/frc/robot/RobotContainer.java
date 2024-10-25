c0725cd296633a4d7711494ec8ab7c1e8e2c802f
scootdboot <andersonscott100@gmail.com>
2024-10-24 20:26:06 -0400

    Delete unused imports, files

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,8 +5,6 @@
 package frc.robot;
 
 import frc.robot.commands.BeginnerAuton;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.commands.TankDrive;
 import frc.robot.subsystems.XRPDrivetrain;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
--- src/main/java/frc/robot/commands/BeginnerAuton.java
+++ src/main/java/frc/robot/commands/BeginnerAuton.java
@@ -1,7 +1,6 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.subsystems.XRPDrivetrain;
 
