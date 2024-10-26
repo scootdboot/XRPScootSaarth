28f3041f1f5218934e80f320699ed41aabfc38ec
scootdboot <andersonscott100@gmail.com>
2024-10-25 21:45:23 -0400

    Remove unused imports

--- src/main/java/frc/robot/AutonHandler.java
+++ src/main/java/frc/robot/AutonHandler.java
@@ -1,10 +1,5 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
-import frc.robot.subsystems.XRPDrivetrain;
-
 /**
  * Static class
  */
